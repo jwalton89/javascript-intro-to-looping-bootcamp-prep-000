8be130cb13293c80acf45053003880e02a57b2e9
Jeff Walton <jwalton89@gmail.com>
2017-12-10 16:28:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
   if(i === 1) {
     array.push("I am 1 strange loop.")
   } else {
-    array.push("I am ${i} strange loops.")
+    array.push(`I am ${i} strange loops.`)
   }
 return array
 }
@@ -26,5 +26,5 @@ function doWhileLoop(array) {
     array.shift()
     return array
   } while (array.length > 0 && maybeTrue());
-  return array 
+  return array
 }
