509633277787d3a0425bcdada97f5adfb2aea38d
Jeff Walton <jwalton89@gmail.com>
2017-12-10 16:22:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,13 @@ function whileLoop(n) {
 }
 return 'done'
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  array.shift()
+  return array 
+ } while (array.length > 0 && maybeTrue());
+ return array
